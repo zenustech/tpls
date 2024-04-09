50c8fd67889b2b10010c74cc52859fcf3b908b13
littlemine <wxlwxl1993@zju.edu.cn>
2024-04-09 15:25:13 +0800

    allow heterogenous cuda archs

--- CMake/compile_tests/cuda_compute_capability.cc
+++ CMake/compile_tests/cuda_compute_capability.cc
@@ -10,19 +10,48 @@ int main() {
     std::cout << "[cuInit] CUDA error: " << result << '\n';
     return -1;
   }
-  int major, minor;
-  result = cuDeviceGetAttribute(&minor, CU_DEVICE_ATTRIBUTE_COMPUTE_CAPABILITY_MINOR, (CUdevice)0);
+
+  int numTotalDevice = 0;
+  result = cuDeviceGetCount(&numTotalDevice);
   if (result != CUDA_SUCCESS) {
-    std::cout << "[cuDeviceGetAttribute: minor] CUDA error: " << result << '\n';
+    std::cout << "[cuDeviceGetCount] CUDA error: " << result << '\n';
+    return -1;
+  } else if (numTotalDevice == 0) {
+    std::cout << "No cuda device detected" << '\n';
     return -1;
   }
-  result = cuDeviceGetAttribute(&major, CU_DEVICE_ATTRIBUTE_COMPUTE_CAPABILITY_MAJOR, (CUdevice)0);
-  if (result != CUDA_SUCCESS) {
-    std::cout << "[cuDeviceGetAttribute: major] CUDA error: " << result << '\n';
-    return -1;
+
+  for (int i = 0; i < numTotalDevice; ++i) {
+    CUdevice dev;
+    CUcontext ctx;
+    cuDeviceGet(&dev, i);
+#if 0
+    result = cuDevicePrimaryCtxRetain(&ctx, dev);
+    if (result != CUDA_SUCCESS) {
+      std::cout << "[cuDevicePrimaryCtxRetain] CUDA error: " << result << '\n';
+      return -1;
+    }
+    result = cuCtxSetCurrent(ctx);
+    if (result != CUDA_SUCCESS) {
+      std::cout << "[cuCtxSetCurrent] CUDA error: " << result << '\n';
+      return -1;
+    }
+#endif
+
+    int major, minor;
+    result = cuDeviceGetAttribute(&minor, CU_DEVICE_ATTRIBUTE_COMPUTE_CAPABILITY_MINOR, dev);
+    if (result != CUDA_SUCCESS) {
+      std::cout << "[cuDeviceGetAttribute: minor] CUDA error: " << result << '\n';
+      return -1;
+    }
+    result = cuDeviceGetAttribute(&major, CU_DEVICE_ATTRIBUTE_COMPUTE_CAPABILITY_MAJOR, dev);
+    if (result != CUDA_SUCCESS) {
+      std::cout << "[cuDeviceGetAttribute: major] CUDA error: " << result << '\n';
+      return -1;
+    }
+    unsigned int const compute_capability = major * 10 + minor;
+    std::cout << compute_capability << ' ';
   }
-  unsigned int const compute_capability = major * 10 + minor;
-  std::cout << compute_capability;
 
   return 0;
 }
